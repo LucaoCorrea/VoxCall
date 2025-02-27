80f4c247ceb2be31562e285eed3cea32a02ad6f7
Lucas Corrêa <99729329+LucaoCorrea@users.noreply.github.com>
2025-02-27 15:38:02 -0300

    auth | mysql conn

---
+++ auth.py
@@ -0,0 +1,32 @@
+import json
+import os
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+
+SCOPES = [
+    "https://www.googleapis.com/auth/userinfo.email",
+    "https://www.googleapis.com/auth/userinfo.profile",
+    "openid"
+]
+
+TOKEN_PATH = "db/token.json"
+CREDENTIALS_PATH = "client/client_secrets.json"
+
+def authenticate_google():
+    creds = None
+
+    if os.path.exists(TOKEN_PATH):
+        creds = Credentials.from_authorized_user_file(TOKEN_PATH, SCOPES)
+
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(CREDENTIALS_PATH, SCOPES)
+            creds = flow.run_local_server(port=0)
+
+        with open(TOKEN_PATH, "w") as token:
+            token.write(creds.to_json())
+
+    return creds
